0a84f666b2a752e6214bf3cff2b7a22b7b5370c0
Lars G Johnsen <yoonsen@gmail.com>
2021-08-25 22:54:06 +0200

    ed

--- ngram_day.py
+++ ngram_day.py
@@ -5,6 +5,8 @@ import dhlab_v2 as d2
 import pandas as pd
 from PIL import Image
 
+import datetime
+
 
 @st.cache(suppress_st_warning=True, show_spinner = False)
 def sumword(words, period):
@@ -21,11 +23,11 @@ def sumword(words, period):
 @st.cache(suppress_st_warning=True, show_spinner = False)
 def ngram(word, period, smooth_slider, sammenlign):
     
-    res = d2.ngram_news(word, period = (period_slider[0], period_slider[1]))
+    res = d2.ngram_news(word, period = period)
     res.index = res.index.map(pd.Timestamp)
     
     if sammenlign != "":
-        tot = sumword(sammenlign, period=(period_slider[0], period_slider[1]))
+        tot = sumword(sammenlign, period = period)
         for x in res:
             res[x] = res[x]/tot
     
@@ -47,39 +49,37 @@ st.markdown('### Trendlinjer')
 ################################### Sammenlign ##################################
 
 st.sidebar.header('Input')
-words = st.text_input('Fyll inn ord eller bigram adskilt med komma. Det skilles mellom store og små bokstaver', "frihet")
+words = st.text_input('Skriv inn ett eller flere enkeltord adskilt med komma. Det er forskjell på store og små bokstaver', "frihet")
 
 sammenlign = st.sidebar.text_input("Sammenlingn med summen av en liste med ord. Om listen ikke er tom viser y-aksen antall forekomster pr summen av ordene det sammenlignes med. Er listen tom viser y-aksen antall forekomster", "")
+
+allword = list(set([w.strip() for w in words.split(',')]))[:30]
 
 
 #################################### Glatting ############################################
 
 st.sidebar.header('Visning')
-smooth_slider = st.sidebar.slider('Glatting', 1, 21, 7)
+smooth_slider = st.sidebar.slider('Glatting', 1, 21, 3)
 
-
-
-allword = list(set([w.strip() for w in words.split(',')]))[:30]
-
-
-
-import datetime
-
-today = datetime.date.today()
-tomorrow = today + datetime.timedelta(days = 1)
 
 #########################  Angi periode ##################################
+
 st.sidebar.title("Periode")
-st.sidebar.write("Velg periode. Angi siste dato av en periode på 3 år")
+
+st.sidebar.write("Velg lengde på periode og midt-dato")
+mid_date = st.sidebar.date_input('Dato', datetime.datetime.today() - datetime.timedelta(days = 183))
+period_size = st.sidebar.number_input("Antall dager før og etter, maks 730, minimum 100", min_value= 100, max_value = 730, value = 183)
 
 
-end_date = st.sidebar.date_input('', today)
-start_date = end_date - datetime.timedelta(days = 1100)
 
-delta = end_date - start_date
-period_slider = (start_date.strftime("%Y%m%d"), end_date.strftime("%Y%m%d"))
+### Beregn start og slutt, og vis frem graf
 
-df = ngram(allword, (period_slider[0], period_slider[1]), smooth_slider, sammenlign)
+start_date = mid_date - datetime.timedelta(days = period_size)
+end_date = mid_date + datetime.timedelta(days = period_size)
+
+period = (start_date.strftime("%Y%m%d"), end_date.strftime("%Y%m%d"))
+#st.write(mid_date, period)
+df = ngram(allword, period, smooth_slider, sammenlign)
 st.line_chart(df)
 
 
